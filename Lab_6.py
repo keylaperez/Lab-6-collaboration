e593900593e452976afb119c38d5c7b4d51d3744
Keyla Perez <keylaperez@Keylas-Laptop.local>
2024-03-20 13:46:38 -0400

    added comments to make easier and removed unecessary comments

--- Lab_6.py
+++ Lab_6.py
@@ -3,12 +3,6 @@
 def encode(password):
     """
     Encode the password by shifting each digit up by 3 numbers.
-
-    Args:
-    password (str): The 8-digit password in string format containing only integers.
-
-    Returns:
-    str: The encoded password.
     """
     encoded_password = ""
     for digit in password:
